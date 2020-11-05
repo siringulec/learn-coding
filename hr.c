e748ff6b0dfcf3b4d9528f7234331157e9e7c518
siringulec <siringulec1@gmail.com>
2020-11-05 19:55:49 +0300

    Resolve conflicts

---
+++ hr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*
+int main(int argc, char const *argv[])
+{ 
+    int t, *A, i, j, tmp;
+
+    scanf("%d", &t);
+
+    for ( i = 0; i < t; i++)
+    {
+        scanf("%d", A + i);
+    }
+
+    j = t - 1;
+
+    for(i = 0; i < t/2; i++)
+    {
+        tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+        j -= 1;
+    }
+
+    for(i = 0; i < t; i++)
+    {
+        printf("%d ", *(A + i));
+    }
+    
+    return 0;
+}*/
