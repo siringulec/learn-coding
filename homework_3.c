5c978b4d0a3f0293a3c98becf4a4e67e936d3164
siringulec <siringulec1@gmail.com>
2020-12-31 00:06:14 +0300

    homework

--- homework_3.c
+++ homework_3.c
@@ -5,7 +5,6 @@
 int first_dice_roll(void);
 int roll_to_win(int n);
 
-
 int main(void)
 {
     int roll, win_roll, try = 1;
@@ -21,7 +20,7 @@ int main(void)
         printf("Let's start!!\n");
     }
     else if(start == 'n'){
-        printf("Okayyy :(\n");
+        printf("Okay :(\n");
         exit (0);
     }
     else{
@@ -29,11 +28,20 @@ int main(void)
         exit (0);
     }
 
-    win_roll = first_dice_roll();
+    roll = first_dice_roll();
+
+    if(roll == 7 || roll == 11){
+        printf("You won at fist try!!!\n");
+        exit (0);
+    }
+    else if(roll == 2 || roll == 3 || roll == 12){
+        printf("You lost :(\n");
+        exit (0);
+    }
+    
+    win_roll = roll;
     printf("Now you need to roll a %d to win.\n", win_roll);
-
     try = roll_to_win(win_roll);
-
     printf("It took you %d tries to win.\n", try);
 
     return 0;
@@ -46,23 +54,10 @@ int first_dice_roll(void)
     dice1 = 1 + rand()%6;
     dice2 = 1 + rand()%6;
     roll = dice1 + dice2;    
+    printf("You rolled: %d + %d = %d\n", dice1, dice2, roll);  
 
-    printf("Your rolled: %d + %d = %d\n", dice1, dice2, roll);
-    
-    if(roll == 7 || roll == 11){
-        printf("You won at fist try!!!\n");
-        exit (0);
-    }
-    else if(roll == 2 || roll == 3 || roll == 12){
-        printf("You lost :(\n");
-        exit (0);
-    }
-    else{
-        win_roll = roll;
-        return win_roll;
-    }
+    return roll;
 }
-
 
 int roll_to_win(int n)
 {
@@ -72,7 +67,7 @@ int roll_to_win(int n)
         dice1 = 1 + rand()%6;
         dice2 = 1 + rand()%6;
         roll = dice1 + dice2;
-        printf("Your rolled: %d + %d = %d\n", dice1, dice2, roll);
+        printf("You rolled: %d + %d = %d\n", dice1, dice2, roll);
         i++;
     } while (roll != n);
     
