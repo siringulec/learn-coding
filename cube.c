1c33b4a3ba73f8e38e7643f41f8e7963230b03af
siringulec <siringulec1@gmail.com>
2020-12-30 23:36:39 +0300

    homework

--- cube.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("   * * * * *\n  *   .   **\n *       * *\n* * * * *  *\n*   .   *  *\n*  ..   * *\n*       **\n* * * * *\n");
-    return 0;
-}
--- homework_3.c
+++ homework_3.c
@@ -2,12 +2,17 @@
 #include<stdlib.h>
 #include<time.h>
 
-int dice_roll(void);
-void draw_dice(void);
+int first_dice_roll(void);
+int roll_to_win(int n);
+
 
 int main(void)
 {
+    int roll, win_roll, try = 1;
     char start, dummy;
+
+    srand((unsigned) time(0));
+
     printf("Start the game [y, n]:");
     scanf("%c", &start);
     scanf("%c", &dummy);
@@ -20,23 +25,56 @@ int main(void)
         exit (0);
     }
     else{
-        printf("Invalid\n");
+        printf("Invalid entry.\n");
         exit (0);
     }
+
+    win_roll = first_dice_roll();
+    printf("Now you need to roll a %d to win.\n", win_roll);
+
+    try = roll_to_win(win_roll);
+
+    printf("It took you %d tries to win.\n", try);
 
     return 0;
 }
 
-int dice_roll(void)
+int first_dice_roll(void)
 {
-    int dice1, dice2;
-    srand((unsigned) time(0));
+    int dice1, dice2, roll, win_roll;
+
     dice1 = 1 + rand()%6;
     dice2 = 1 + rand()%6;
-    return dice1 + dice2;
+    roll = dice1 + dice2;    
+
+    printf("Your rolled: %d + %d = %d\n", dice1, dice2, roll);
+    
+    if(roll == 7 || roll == 11){
+        printf("You won at fist try!!!\n");
+        exit (0);
+    }
+    else if(roll == 2 || roll == 3 || roll == 12){
+        printf("You lost :(\n");
+        exit (0);
+    }
+    else{
+        win_roll = roll;
+        return win_roll;
+    }
 }
 
-void draw_dice(void)
-{
 
+int roll_to_win(int n)
+{
+    int dice1, dice2, roll, i = 1;
+
+    do{
+        dice1 = 1 + rand()%6;
+        dice2 = 1 + rand()%6;
+        roll = dice1 + dice2;
+        printf("Your rolled: %d + %d = %d\n", dice1, dice2, roll);
+        i++;
+    } while (roll != n);
+    
+    return i;
 }
